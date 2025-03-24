2c8a43365a2e720cbda957a4a391997a7bd8f170
Helene Tharrault <htharrau@student.42berlin.de>
2025-03-24 17:52:39 +0100

    small changes

--- src_bonus/raycasting/g_draw_miniray.c
+++ src_bonus/raycasting/g_draw_miniray.c
@@ -6,7 +6,7 @@
 /*   By: htharrau <htharrau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 20:37:40 by htharrau          #+#    #+#             */
-/*   Updated: 2025/03/24 17:29:17 by htharrau         ###   ########.fr       */
+/*   Updated: 2025/03/24 17:45:59 by htharrau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,70 @@ static void	draw_line(t_data *data, t_coord *p1, t_coord *delta)
 		if (delta->x < 0)
 			x1 = floor(p1->x);
 		else
+			x1 = floor(p1->x);
+		if (delta->y < 0)
+			y1 = floor(p1->y);
+		else
+			y1 = floor(p1->y);
+		put_pixel_minimap(data, x1, y1, RAY_COL);
+		p1->x += delta->x;
+		p1->y += delta->y;
+		i++;
+	}
+}
+
+/* static void	draw_vertical(t_data *data, t_coord *p1, t_coord *p2)
+{
+	int	x;
+	int	y;
+	int	y1;
+	int	y2;
+
+	x = round(p1->x);
+	y1 = round(fmin(p1->y, p2->y));
+	y2 = round(fmax(p1->y, p2->y));
+	y = y1;
+	while (y <= y2)
+	{
+		put_pixel_minimap(data, x, y, RAY_COL);
+		y++;
+	}
+}
+
+static void	draw_horizontal(t_data *data, t_coord *p1, t_coord *p2)
+{
+	int	x;
+	int	y;
+	int	x1;
+	int	x2;
+
+	y = round(p1->y);
+	x1 = round(fmin(p1->x, p2->x));
+	x2 = round(fmax(p1->x, p2->x));
+	x = x1;
+	while (x <= x2)
+	{
+		put_pixel_minimap(data, x, y, RAY_COL);
+		x++;
+	}
+}
+
+static void	draw_line(t_data *data, t_coord *p1, t_coord *delta)
+{
+	int		i;
+	int		x1;
+	int		y1;
+	float	step;
+
+	step = fmaxf(fabsf(delta->x), fabsf(delta->y));
+	delta->x /= step;
+	delta->y /= step;
+	i = 0;
+	while (i <= step)
+	{
+		if (delta->x < 0)
+			x1 = floor(p1->x);
+		else
 			x1 = ceil(p1->x);
 		if (delta->y < 0)
 			y1 = floor(p1->y);
@@ -112,3 +176,4 @@ static void	draw_line(t_data *data, t_coord *p1, t_coord *delta)
 		i++;
 	}
 }
+ */
