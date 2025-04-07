0befbd73d57a1dee8d0998edd6b02fd8a25fc87b
Helene Tharrault <htharrau@student.42berlin.de>
2025-04-07 11:54:34 +0200

    clear image before redrawing (ft_memset)

--- src/raycasting/b_loop_hook.c
+++ src/raycasting/b_loop_hook.c
@@ -6,7 +6,7 @@
 /*   By: htharrau <htharrau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 22:43:01 by htharrau          #+#    #+#             */
-/*   Updated: 2025/03/21 15:48:07 by htharrau         ###   ########.fr       */
+/*   Updated: 2025/04/07 11:53:06 by htharrau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void	hoop_func(void *param)
 		update_player(data);
 		if (data->flag_refresh == true)
 		{
+			ft_memset(data->img->pixels, 0, 
+				data->img->width * data->img->height * sizeof(int));
 			draw_ceiling_floor(data);
 			draw_walls(data);
 			data->flag_refresh = false;
--- src_bonus/raycasting/b_loop_hook.c
+++ src_bonus/raycasting/b_loop_hook.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   b_loop_hook.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilazar <ilazar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: htharrau <htharrau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 22:43:01 by htharrau          #+#    #+#             */
-/*   Updated: 2025/04/01 18:44:38 by ilazar           ###   ########.fr       */
+/*   Updated: 2025/04/07 11:52:57 by htharrau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	hoop_func(void *param)
 		update_fov(data);
 		if (data->flag_refresh == true)
 		{
+			ft_memset(data->img->pixels, 0, 
+				data->img->width * data->img->height * sizeof(int));
 			draw_ceiling_floor(data);
 			draw_walls(data);
 			draw_minimap(data);
